20943c53cf3dc938b247d3da0308b88f64924d38
nzwh <notzerowh@gmail.com>
2025-12-04 21:50:38 +0800

    feat: dashboard interface

---
+++ frontend/lib/functions/FormatName.ts
@@ -0,0 +1,12 @@
+const FormatName = (name: string | undefined): string => {
+  if (!name) 
+    return 'User';
+
+  return name
+    .replace(/[._-]/g, ' ')
+    .split(' ')
+    .map(word => word.charAt(0).toUpperCase() + word.slice(1).toLowerCase())
+    .join(' ');
+}
+
+export default FormatName;
